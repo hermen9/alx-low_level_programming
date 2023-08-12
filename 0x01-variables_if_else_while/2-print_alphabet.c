b8358417bc2dbe9e48b710844f6722d63584537b
Donald Koronya <dkoronya246@gmail.com>
2023-08-12 01:34:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - executes alphabet code
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char alphabet = "a";
+
+	while (alphabet <= "z")
+	{
+		putchar(alphabet);
+		alphabet++
+	}
+	putchar("\n")
+	return (0);
+}
